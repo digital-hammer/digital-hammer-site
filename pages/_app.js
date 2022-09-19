a6b2b5aad561bf42fb4fc89bc9d44db5dbcf1ee8
icarus64 <ellishogan95@gmail.com>
2022-09-18 22:59:27 -0400

    first update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import {useState, useEffect} from 'react'
+import Head from 'next/head'
+import Header from 'sections/header'
+import Footer from 'sections/footer'
+import 'styles/_appDefault.scss'
+import 'styles/fontawesome-all.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [width, setWidth] = useState(0)
+  useEffect(() => {
+    const w = () => setWidth(window.innerWidth)
+    w()
+    window.addEventListener('resize', w)
+    return () => window.removeEventListener('resize', w)
+  }, [width])
+
+  return (
+    <>
+      <Head>
+        <title>The Digital Hammer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header width={width} />
+      <Component {...pageProps} width={width} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
