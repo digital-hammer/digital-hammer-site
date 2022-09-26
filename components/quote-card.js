f931e4ed5ee642ba1299f1bddd3b1bc114e72c91
icarus64 <ellishogan95@gmail.com>
2022-09-26 16:05:59 -0400

    update

--- components/quote-card.js
+++ components/quote-card.js
@@ -1,13 +1,61 @@
-export default (props)=> {
+export default (props) => {
   const {
-    content
+    options=[],
+    onChange
   } = props || {}
 
   return (
     <form>
-      {/*content.map((items)=> {
-        
-      })*/}
+      {options.map((item) => {
+        const {
+          type = "",
+          data
+        } = item;
+        switch (type.toLowerCase()) {
+          case "select": {
+            const {name, options} = data
+            return (
+              <select name={name} onChange={(e) => onChange({})}>
+                {options.map((el)=> {
+                  <option value={el.value}>{el.value}</option>
+                })}
+              </select>
+            )
+          }
+
+          case "date": {
+
+            return <input />
+          }
+
+
+          case "radio": {
+
+            return (
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            )
+          }
+          
+          case 'service box': {
+
+            return (
+              <div className="service-box">
+
+              </div>
+            )
+
+          }
+
+          case "text":
+          default: {
+
+            return <input />
+          }
+        }
+      })}
     </form>
   )
 }
