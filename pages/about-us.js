997c26e873b2055ef85b6a957657b7990a280ab1
josephc2195 <chicaj1221@gmail.com>
2022-09-27 18:31:00 -0400

    Update about-us.js
    added short about me

--- pages/about-us.js
+++ pages/about-us.js
@@ -5,15 +5,19 @@ const AboutUsPage = () => {
         <header className="major">
           <h2>About Us</h2>
           <p>Welcome to Digital Hammer</p>
-          <p>Digital Hammer is a web-development company that offers ultra fast websites at a cost well below market.</p>
+          <p>Digital Hammer is a web-development company that offers ultra fast websites at a cost well below market. 
+            We also offer other types of websites and will work with you to get you a product to meet your specifications
+
+          </p>
         </header>
         <div className="content">
 
-          <h3>Lorem ipsum dolor</h3>
-          <p>Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor. Duis vulputate ex et ex tincidunt, quis lacinia velit aliquet. Duis non efficitur nisi, id malesuada justo. Maecenas sagittis felis ac sagittis semper. Curabitur purus leo, tempus sed finibus eget, fringilla quis risus. Maecenas et lorem quis sem varius sagittis et a est. Maecenas iaculis iaculis sem. Donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Etiam ut gravida sapien.</p>
-
-          <p>Vestibulum ultrices risus velit, sit amet blandit massa auctor sit amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis id vel diam. Praesent sit amet posuere risus, eu faucibus lectus. Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam. Proin nec dui cursus, posuere dui eget interdum. Fusce lectus magna, sagittis at facilisis vitae, pellentesque at etiam. Quisque posuere leo quis sem commodo, vel scelerisque nisi scelerisque. Suspendisse id quam vel tortor tincidunt suscipit. Nullam auctor orci eu dolor consectetur, interdum ullamcorper ante tincidunt. Mauris felis nec felis elementum varius.</p>
-
+          <h2>The people that built Digital Hammer</h2>
+          <h1>Joseph Chica,</h1>
+          <h3>CEO and Co-Founder</h3>
+          <p>As a UNCC alumnus, I graduated with a Bachelor of Science in Computer science with a concentration in Software, Systems, and Networks. 
+            While in school I frequently completed side projects that involved web development. Now I have been developing software as a professional for a year. 
+            I believe strong values such as leadership, talent, and hard work are the keys to success, and those are the qualities Digital Hammer can provide.  </p>
           <hr />
 
           <h4>Feugiat aliquam</h4>
@@ -22,7 +26,6 @@ const AboutUsPage = () => {
           <p>Etiam iaculis nulla ipsum, et pharetra libero rhoncus ut. Phasellus rutrum cursus velit, eget condimentum nunc blandit vel. In at pulvinar lectus. Morbi diam ante, vulputate et imperdiet eget, fermentum non dolor. Ut eleifend sagittis tincidunt. Sed viverra commodo mi, ac rhoncus justo. Duis neque ligula, elementum ut enim vel, posuere finibus justo. Vivamus facilisis maximus nibh quis pulvinar. Quisque hendrerit in ipsum id tellus facilisis fermentum. Proin mauris dui, at vestibulum sit amet, auctor bibendum neque.</p>
         </div>
       </section>
-
     </div>
   )
 }
