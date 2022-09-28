acb1e5962b889c92f0dcce1466b1c959d9949bf9
icarus64 <ellishogan95@gmail.com>
2022-09-28 03:47:52 -0400

    update

--- src/sections/profile-cards.js
+++ src/sections/profile-cards.js
@@ -7,7 +7,7 @@ export default (props) => {
       {
         image: "images/profile/joseph.jpg",
         name: "Joseph Chica",
-        title: "Co-Founder, CEO",
+        title: "Co-Owner, CEO",
         info: "As a UNCC alumnus, I graduated with a Bachelor of Science in Computer science with a concentration in Software, Systems, and Networks. I have been developing software as a professional for several years.",
         links: {
           facebook: "https://www.facebook.com/mr.jnc/",
@@ -18,7 +18,7 @@ export default (props) => {
       {
         image: "images/profile/ellis.jpg",
         name: "Ellis Hogan",
-        title: "Co-Founder, CTO",
+        title: "Co-Owner, CTO",
         info: "I'm a self taught developer of 6 years with skills in all things frontend. I have a expertise in Javascript, React, HTML, CSS, Shopify, SEO as well as best practices for web design, development, and hosting.",
         links: {
           facebook: "https://www.facebook.com/ellis.hogan.1",
