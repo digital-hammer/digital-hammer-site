ee389dd16aad3e3623a627a351cd2aa02dc9c9ec
icarus64 <ellishogan95@gmail.com>
2022-09-27 21:55:57 -0400

    update

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,3 +1,4 @@
+import ProfileCards from "src/sections/profile-cards"
 const AboutUsPage = () => {
   return (
     <div id="main">
@@ -7,22 +8,12 @@ const AboutUsPage = () => {
           <p>Welcome to Digital Hammer</p>
         </header>
         <div className="content">
-          <h3>Digital Hammer is a web-development company that offers ultra fast websites at a cost well below market. 
+          <h3>
+            Digital Hammer is a web-development company that offers ultra fast websites at a cost well below market. 
             We also offer other types of websites and will work with you to get you a product to meet your specifications
           </h3>
-          <h2>The people that built Digital Hammer</h2>
-          <h1>Joseph Chica,</h1>
-          <h3>CEO and Co-Founder</h3>
-          <p>As a UNCC alumnus, I graduated with a Bachelor of Science in Computer science with a concentration in Software, Systems, and Networks. 
-            While in school I frequently completed side projects that involved web development. Now I have been developing software as a professional for a year. 
-            I believe strong values such as leadership, talent, and hard work are the keys to success, and those are the qualities Digital Hammer can provide.  </p>
-          <hr />
-
-          <h4>Feugiat aliquam</h4>
-          <p>Nam sapien ante, varius in pulvinar vitae, rhoncus id massa. Donec varius ex in mauris ornare, eget euismod urna egestas. Etiam lacinia tempor ipsum, sodales porttitor justo. Aliquam dolor quam, semper in tortor eu, volutpat efficitur quam. Fusce nec fermentum nisl. Aenean erat diam, tempus aliquet erat.</p>
-
-          <p>Etiam iaculis nulla ipsum, et pharetra libero rhoncus ut. Phasellus rutrum cursus velit, eget condimentum nunc blandit vel. In at pulvinar lectus. Morbi diam ante, vulputate et imperdiet eget, fermentum non dolor. Ut eleifend sagittis tincidunt. Sed viverra commodo mi, ac rhoncus justo. Duis neque ligula, elementum ut enim vel, posuere finibus justo. Vivamus facilisis maximus nibh quis pulvinar. Quisque hendrerit in ipsum id tellus facilisis fermentum. Proin mauris dui, at vestibulum sit amet, auctor bibendum neque.</p>
         </div>
+        <ProfileCards />
       </section>
     </div>
   )
