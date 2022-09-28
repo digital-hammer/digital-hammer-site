bf26ddadb2829dafde39f202018b5176e7c8c9a6
icarus64 <ellishogan95@gmail.com>
2022-09-28 00:54:24 -0400

    update

--- pages/about-us.js
+++ pages/about-us.js
@@ -9,10 +9,12 @@ const AboutUsPage = () => {
             <p>Welcome to Digital Hammer</p>
           </header>
           <div className="content">
-            <h3>
+            <h3>Who We Are</h3>
+            <p>
               Digital Hammer is a web-development company that offers ultra fast websites at a cost well below market.
-              We also offer other types of websites and will work with you to get you a product to meet your specifications
-            </h3>
+              We also offer other types of websites and will work with you to get you a product to meet your specifications. 
+            </p>
+            <h3>A Brief History</h3>
           </div>
         </section>
       </div>
