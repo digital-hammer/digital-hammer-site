5e667dac6b63c3a1a825159dcc469da46fb33a6b
icarus64 <ellishogan95@gmail.com>
2022-10-01 03:58:27 -0400

    update

--- pages/get-quote.js
+++ pages/get-quote.js
@@ -120,6 +120,7 @@ const GetQuote = (props) => {
 						<input type="hidden" name="quote" value="quote" />
 						{forms[position]}
 						<textarea className="hidden" name="Info" value={formattedInfo} />
+						<textarea className="hidden" name="Json Info" value={<pre>{JSON.stringify({contactInfo, selections, linePrices, totalPrice})}</pre>} />
 						<div className="definition">
 							{definition}
 						</div>
