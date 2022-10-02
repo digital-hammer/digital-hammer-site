39136a7fd4a68d04237aec56174e00dadc5abd5d
icarus64 <ellishogan95@gmail.com>
2022-10-02 01:39:02 -0400

    update

--- pages/success.js
+++ pages/success.js
@@ -1,6 +1,8 @@
 const Success = (props) => {
 	return (
 		<div className="submit-success">
+			<h1>Success</h1>
+			<h3>Thank you for your submission we will get back to you shortly.</h3>
 			<img src="/images/undraw/certification.svg" alt="" />
 		</div>
 	)
